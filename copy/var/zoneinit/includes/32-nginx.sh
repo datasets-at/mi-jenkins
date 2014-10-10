f29910b6e28df0333f052db1e4f82ff5809f45e1
Thomas Merkel <tm@core.io>
2014-10-10 11:07:19 +0200

    Only use nginx ssl proxy if mdata variable nginx_ssl is set
    This allow you to run jenkins without an ssl proxy on the local machine. It's still recommended to use ssl, but maybe you've an proxy somewhere else. The Jenkins SMF script is automatically configured to listen on localhost or on all IP addresses. For that reason the SMF script has been modified to use an default instance. This allow us to configure it via svccfg properties.

--- copy/var/zoneinit/includes/32-nginx.sh
+++ copy/var/zoneinit/includes/32-nginx.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/bash
 # configure ssl for nginx
 
-host=$(mdata-get sdc:hostname)
 cert_dir='/opt/local/etc/nginx/ssl/'
 
 # SSL
-log "nginx ssl setup"
 if mdata-get nginx_ssl 1>/dev/null 2>&1; then
-	mdata-get nginx_ssl > ${cert_dir}nginx.pem
-else
-	openssl req -newkey rsa:2048 -keyout ${cert_dir}nginx.key \
-				-out ${cert_dir}nginx.csr -nodes \
-				-subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${host}"
-	openssl x509 -in ${cert_dir}nginx.csr -out ${cert_dir}nginx.crt -req -signkey ${cert_dir}nginx.key -days 128
-	cat ${cert_dir}nginx.crt ${cert_dir}nginx.key > ${cert_dir}nginx.pem
-fi
-chmod 400 ${cert_dir}nginx.pem
+	# reconfigure jenkins to listen only on localhost
+	log "reconfigure jenkins to listen only on localhost"
+	svccfg -s jenkins:default setprop application/http_listen_address = astring: '127.0.0.1'
+	svcadm refresh jenkins:default
+	svcadm restart jenkins:default
 
-# Enable nginx
-log "starting nginx"
-svcadm enable svc:/pkgsrc/nginx:default
+	# copy mdata ssl certificate
+	log "copy mdata ssl certificate"
+	mdata-get nginx_ssl > ${cert_dir}nginx.pem
+	chmod 400 ${cert_dir}nginx.pem
+
+	# Enable nginx
+	log "starting nginx"
+	svcadm enable svc:/pkgsrc/nginx:default
+else
+	# reconfigure jenkins to listen only on localhost
+	log "reconfigure jenkins to listen only on localhost"
+	svccfg -s jenkins:default setprop application/http_listen_address = astring: '0.0.0.0'
+	svcadm refresh jenkins:default
+	svcadm restart jenkins:default
+
+	# Disable nginx
+	log "disable nginx"
+	svcadm disable svc:/pkgsrc/nginx:default
+fi
+
