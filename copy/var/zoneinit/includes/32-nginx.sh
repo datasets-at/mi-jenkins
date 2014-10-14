b23a00c589c8621e9aebbfbe73baa462172c8c4c
Daniel Malon <daniel.malon@me.com>
2014-10-14 18:59:18 +0100

    only force ssl if metadata is provided
    still use nginx as a reverse proxy for pure http

--- copy/var/zoneinit/includes/32-nginx.sh
+++ copy/var/zoneinit/includes/32-nginx.sh
@@ -7,15 +7,15 @@ cert_dir='/opt/local/etc/nginx/ssl/'
 # SSL
 log "nginx ssl setup"
 if mdata-get nginx_ssl 1>/dev/null 2>&1; then
-	mdata-get nginx_ssl > ${cert_dir}nginx.pem
-else
-	openssl req -newkey rsa:2048 -keyout ${cert_dir}nginx.key \
-				-out ${cert_dir}nginx.csr -nodes \
-				-subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${host}"
-	openssl x509 -in ${cert_dir}nginx.csr -out ${cert_dir}nginx.crt -req -signkey ${cert_dir}nginx.key -days 128
-	cat ${cert_dir}nginx.crt ${cert_dir}nginx.key > ${cert_dir}nginx.pem
+  mkdir -p ${cert_dir} || true
+  mdata-get nginx_ssl > ${cert_dir}nginx.pem
+  chmod 400 ${cert_dir}nginx.pem
+
+  log "switching jenkins site config"
+  gsed -i \
+          -e "s/jenkins.conf/jenkins_ssl.conf/" \
+          /opt/local/etc/nginx/nginx.conf
 fi
-chmod 400 ${cert_dir}nginx.pem
 
 # Enable nginx
 log "starting nginx"
